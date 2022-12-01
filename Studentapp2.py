1923ff2cb7604b8b656da1930d3827a54de3afd6
Babayoo-das <70839066+Babayoo-das@users.noreply.github.com>
2022-12-01 07:15:45 +0300

    Update Studentapp2.py

--- Studentapp2.py
+++ Studentapp2.py
@@ -29,7 +29,46 @@ def home():
 
 @app.route("/about", methods=['GET'])
 def about():
-    return render_template('getstudents.py')
+    select_sql = "select * from student"
+    cursor = conn.cursor()
+    cursor.execute(select_sql)
+    result = cursor.fetchall()
+    p = []
+
+    tbl =  "<tr><td>Student ID</td><td>First Name</td><td>Last Name</td><td>Course Name</td><td>Year of Study</td></tr>"
+    p.append(tbl)
+    for row in result:
+      a = "<tr><td>%s</td>"%row[0]
+      p.append(a)
+      b = "<td>%s</td>"%row[1]
+      p.append(b)
+      c = "<td>%s</td>"%row[2]
+      p.append(c)
+      d = "<td>%s</td></tr>"%row[3]
+      p.append(d)
+      e = "<td>%s</td></tr>"%row[4]
+      p.append(e)
+
+    contents = '''<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+    <html>
+    <head>
+    <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+    <title>Student Details</title>
+    </head>
+    <body>
+    <table>%s</table>
+    </body>
+    </html>'''%(p)
+
+    filename = 'getstudents.html'
+    def main(contents, filename):
+      outfile = open(filename,"w")
+      outfile.write(contents)
+      outfile.close()
+    main(contents, filename)
+    details = webbrowser.open(filename)
+    cursor.close()
+    return details
 
 @app.route("/addstud", methods=['POST'])
 def AddStud():
