a0c74099a35951079794c2ea11a3aa8867b675ea
Babayoo-das <70839066+Babayoo-das@users.noreply.github.com>
2022-12-01 06:57:17 +0300

    Create config.py

---
+++ templates/config.py
@@ -0,0 +1,6 @@
+dbhost = "students.cm8oeomhbnsh.us-east-2.rds.amazonaws.com"
+dbuser = "awsswetha"
+dbpassword = "Sillymango5"
+database = "students"
+dbbucket = "addstudents"
+dbregion = "us-east-2"
